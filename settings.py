47fbfbf5eb7b0f002b6c9e062bb5b414ba3bb078
nrthbnd <asoloshenkova@gmail.com>
2023-10-24 02:14:01 +0300

    Update SQLALCHEMY_DATABASE_URI

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ LOCAL_HOST = 'http://localhost/'
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
