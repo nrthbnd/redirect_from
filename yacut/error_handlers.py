4e8dc65b42b3fe4c680a714c08ecae8ec186e5f7
nrthbnd <asoloshenkova@gmail.com>
2023-10-23 19:52:31 +0300

    Add errorhandlers and Invalidapiusege class

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,6 +1,28 @@
-from flask import render_template
+from flask import jsonify, render_template
 
 from . import app, db
+
+
+class InvalidAPIUsage(Exception):
+    """Ошибки при обращении а API."""
+    status_code = 400
+
+    def __init__(self, message, status_code=None):
+        super().__init__()
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+
+    # Метод для сериализации переданного сообщения об ошибке
+    def to_dict(self):
+        return dict(message=self.message)
+
+
+# Обработчик кастомного исключения для API
+@app.errorhandler(InvalidAPIUsage)
+def invalid_api_usage(error):
+    """Возвращает в ответе текст ошибки и статус-код"""
+    return jsonify(error.to_dict()), error.status_code
 
 
 @app.errorhandler(404)
