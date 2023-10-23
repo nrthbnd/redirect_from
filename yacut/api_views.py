88644b3fb9b56c4b8da68f639fdccfe9464795eb
nrthbnd <asoloshenkova@gmail.com>
2023-10-23 19:54:22 +0300

    Move validation to api_validators

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,6 +1,8 @@
 from flask import jsonify, request
 
 from . import app, db
+from .api_validators import (validate_api_get_url, validate_api_body,
+                             validate_api_data_url, validate_api_short_link)
 from .models import URLMap
 
 
@@ -8,6 +10,7 @@ from .models import URLMap
 def get_urlmap(short_link):
     """Получение оригинальной ссылки по указанному короткому идентификатору."""
     urlmap = URLMap.query.filter_by(short=short_link).first()
+    validate_api_get_url(urlmap)
     return jsonify({'urlmap': urlmap.to_dict()}), 200
 
 
@@ -15,6 +18,11 @@ def get_urlmap(short_link):
 def add_urlmap():
     """Cоздание новой короткой ссылки."""
     data = request.get_json()
+
+    validate_api_body(data)
+    validate_api_data_url(data)
+    validate_api_short_link(data)
+
     new_link = URLMap()
     new_link.from_dict(data)
     db.session.add(new_link)
