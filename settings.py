055f53aed60a86afd9338343db0859b1c3128076
nrthbnd <asoloshenkova@gmail.com>
2023-10-23 18:20:20 +0300

    Add settings for project

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+
+"""Длина ссылки по умолчанию."""
+LENGTH_OF_NEW_LINK = 6
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
