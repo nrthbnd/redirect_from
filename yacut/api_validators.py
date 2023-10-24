f7c1149c6957ac51412eb39ac54ea6ca1827bec2
nrthbnd <asoloshenkova@gmail.com>
2023-10-24 10:40:37 +0300

    Use HTTPStatuses, import regex constant

--- yacut/api_validators.py
+++ yacut/api_validators.py
@@ -1,4 +1,7 @@
 import re
+from http import HTTPStatus
+
+from settings import REGEX_PATTERN
 
 from .error_handlers import InvalidAPIUsage
 from .models import URLMap
@@ -7,30 +10,35 @@ from .models import URLMap
 def validate_api_get_url(urlmap):
     """Проверка наличия запрашиваемой короткой ссылки."""
     if urlmap is None:
-        raise InvalidAPIUsage('Указанный id не найден', 404)
+        raise InvalidAPIUsage(
+            'Указанный id не найден',
+            HTTPStatus.NOT_FOUND)
 
 
 def validate_api_body(data):
     """Проверка наличия тела запроса."""
     if not data:
         raise InvalidAPIUsage(
-            'Отсутствует тело запроса', 400)
+            'Отсутствует тело запроса',
+            HTTPStatus.BAD_REQUEST)
 
 
 def validate_api_data_url(data):
     """Проверка наличия поля с оригинальной ссылкой."""
     if 'url' not in data:
         raise InvalidAPIUsage(
-            '"url" является обязательным полем!', 400)
+            '"url" является обязательным полем!',
+            HTTPStatus.BAD_REQUEST)
 
 
 def validate_api_short_link(data):
     """Проверка корректности создания короткой ссылки."""
-    pattern = r'^[A-Za-z0-9_]{1,16}$'
-    if not re.match(pattern, data['custom_id']):
+    # pattern = r'^[A-Za-z0-9_]{1,16}$'
+    if not re.match(REGEX_PATTERN, data['custom_id']):
         raise InvalidAPIUsage(
             'Указано недопустимое имя для короткой ссылки')
 
     if URLMap.query.filter_by(short=data['custom_id']).first():
         raise InvalidAPIUsage(
-            'Предложенный вариант короткой ссылки уже существует.', 400)
+            'Предложенный вариант короткой ссылки уже существует.',
+            HTTPStatus.BAD_REQUEST)
