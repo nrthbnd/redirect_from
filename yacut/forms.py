3c0bb3d5e010f75aa255afcc6d2614fb99ba94b2
nrthbnd <asoloshenkova@gmail.com>
2023-10-24 10:49:52 +0300

    Add Regexp validator to custom_id field

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, Regexp
+
+from settings import REGEX_PATTERN
 
 
 class URLForm(FlaskForm):
@@ -11,7 +13,7 @@ class URLForm(FlaskForm):
     )
     custom_id = StringField(
         'Ваш вариант короткой ссылки',
-        validators=[Length(1, 16),
+        validators=[Regexp(REGEX_PATTERN),
                     Optional()]
     )
     submit = SubmitField('Создать')
