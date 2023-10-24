9b1338909de422292dea9e00842baf8493fec14c
nrthbnd <asoloshenkova@gmail.com>
2023-10-24 11:34:51 +0300

    Correct docstrings, add HTTPStatuses

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from flask import jsonify, render_template
 
 from . import app, db
@@ -5,7 +7,7 @@ from . import app, db
 
 class InvalidAPIUsage(Exception):
     """Ошибки при обращении а API."""
-    status_code = 400
+    status_code = HTTPStatus.BAD_REQUEST
 
     def __init__(self, message, status_code=None):
         super().__init__()
@@ -13,24 +15,23 @@ class InvalidAPIUsage(Exception):
         if status_code is not None:
             self.status_code = status_code
 
-    # Метод для сериализации переданного сообщения об ошибке
     def to_dict(self):
+        """Сериализовать переданное сообщение об ошибке."""
         return dict(message=self.message)
 
 
-# Обработчик кастомного исключения для API
 @app.errorhandler(InvalidAPIUsage)
 def invalid_api_usage(error):
-    """Возвращает в ответе текст ошибки и статус-код"""
+    """Вернуть в ответе текст ошибки и статус-код."""
     return jsonify(error.to_dict()), error.status_code
 
 
-@app.errorhandler(404)
+@app.errorhandler(HTTPStatus.NOT_FOUND)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('404.html'), HTTPStatus.NOT_FOUND
 
 
-@app.errorhandler(500)
+@app.errorhandler(HTTPStatus.INTERNAL_SERVER_ERROR)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('500.html'), HTTPStatus.INTERNAL_SERVER_ERROR
